ee57d03a29095923cce7357e9ab343c2ffdfb0df
jsjackson <jsjackson@example.com>
2021-06-05 23:48:57 +1000

    initial scaffolding

---
+++ src/test/java/software/jsj/microservices/limitsservice/LimitsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package software.jsj.microservices.limitsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
