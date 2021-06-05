7e697ca45973d540c52477f0e5b45752ca188714
jsjackson <jsjackson@example.com>
2021-06-06 00:02:47 +1000

    get limits from configuration file

--- src/main/java/software/jsj/microservices/limitsservice/controller/LimitsController.java
+++ src/main/java/software/jsj/microservices/limitsservice/controller/LimitsController.java
@@ -1,15 +1,21 @@
 package software.jsj.microservices.limitsservice.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import software.jsj.microservices.limitsservice.bean.Limits;
+import software.jsj.microservices.limitsservice.configuration.Configuration;
 
 @RestController
 public class LimitsController {
 
+    @Autowired
+    private Configuration configuration;
+
     @GetMapping("/limits")
     public Limits retrieveLimits() {
-        return new Limits(1, 1000);
+        return new Limits(
+                configuration.getMinimum(), configuration.getMaximum());
     }
 
 }
